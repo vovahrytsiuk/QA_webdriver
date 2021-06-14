5092f515160cb8c126121fda633d2cfb531c5106
vovahrytsiuk <vovaoffonua@gmail.com>
2021-06-14 10:18:38 +0300

    unit test realisation

--- lab6/venv/main.py
+++ lab6/venv/main.py
@@ -1,24 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+import unittest
+import time
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("https://github.com/")
-search = driver.find_element_by_name('q')
-search.send_keys("selenium python")
-search.send_keys(Keys.RETURN)
-tag = driver.find_elements_by_class_name("topic-tag")
-tag = [i.text for i in tag]
-assert 'python' in tag and 'webdriver' in tag and 'selenium' in tag
-star = driver.find_element_by_css_selector(".starring-container > a").click()
-title = driver.find_element_by_css_selector('h1')
-assert title.text == 'Sign in to GitHub'
-username = driver.find_element_by_name('login')
-username.send_keys('username')
-password = driver.find_element_by_name('password')
-password.send_keys('qwerty123')
-password.send_keys(Keys.RETURN)
-error = driver.find_element_by_xpath('//*[@id="login"]/p')
-assert error.text == 'There have been several failed attempts to sign in ' \
-                     'from this account or IP address. Please wait a while and try again later.'
-driver.close()
+
+class GitTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.get("https://github.com/")
+
+    def test_01(self):
+        # check for existing Google title in page google.com
+        assert "Git" in self.driver.page_source
+
+    def test_02(self):
+        search = self.driver.find_element_by_name('q')
+        search.send_keys("selenium python")
+        search.send_keys(Keys.RETURN)
+        tag = self.driver.find_elements_by_class_name("topic-tag")
+        tag = [i.text for i in tag]
+        assert 'python' in tag and 'webdriver' in tag and 'selenium' in tag
+        star = self.driver.find_element_by_css_selector(".starring-container > a").click()
+        title = self.driver.find_element_by_css_selector('h1')
+        assert title.text == 'Sign in to GitHub'
+        username = self.driver.find_element_by_name('login')
+        username.send_keys('new_login')
+        password = self.driver.find_element_by_name('password')
+        password.send_keys('qwerty123')
+        password.send_keys(Keys.RETURN)
+        error = self.driver.find_element_by_xpath('//*[@id="login"]/p')
+        assert error.text == 'There have been several failed attempts to sign in ' \
+                             'from this account or IP address. Please wait a while and try again later.'
+
+    def tearDown(self) -> None:
+        self.driver.quit()
+
+
+
+if __name__ == "__main__":
+    unittest.main()
